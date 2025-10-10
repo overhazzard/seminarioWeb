807f156453625702a9e21b96871f5a076e7ea54c
Wilberd <wilberd.espino99@gmail.com>
2025-10-10 02:34:53 -0500

    Commit

---
+++ script.js
@@ -0,0 +1,14 @@
+new TypeIt("#element", {
+  speed: 50,
+  loop: true,
+}).go();
+
+new TypeIt("#element2", {
+  speed: 50,
+  loop: true,
+}).go();
+
+new TypeIt("#element3", {
+  speed: 40,
+  loop: true,
+}).go();
